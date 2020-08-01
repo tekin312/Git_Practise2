444148a0fa492dec4b5bc4a2d79884e7c7f35ab7
tekin312 <68976913+tekin312@users.noreply.github.com>
2020-08-01 15:34:36 -0500

    Update HelloB20.java

--- gitDemo/src/HelloB20.java
+++ gitDemo/src/HelloB20.java
@@ -1,6 +1,8 @@
 public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("Hello B20,it is my pleasure to be with you!");
+        System.out.ptintln("Happy to be here");
+          
 
 
     }
