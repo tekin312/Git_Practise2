94deab30eacdc438766c8120d7e626c7f35849dd
tekin312 <nkulaksiz@gmail.com>
2020-08-05 19:31:10 -0500

    Hello Reverse.java

---
+++ Summer2020_B20_git/src/Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+
+    public static String reverse(String str){
+        String result = "";
+        for(int i = str.length()-1;  i >=0 ; i--){
+            result += str.charAt(i);
+        }
+        return result;
+    }
+
+}
